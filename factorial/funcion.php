d6f90a8fa7ab6553a1f49c03d76de6d89262dbfb
dsilvaEVG <dsilvavega.guadalupe@alumnado.fundacionloyola.net>
2024-09-25 13:20:34 +0200

    Update funcion.php

--- factorial/funcion.php
+++ factorial/funcion.php
@@ -1,10 +1,10 @@
 <?php
-// Creamos la función del factorial
+//Creamos la funcion que calcula el factorial de un número
 	function factorial($num){
-		if ($num ==0 ) {return 1;}
+		if ($num ==0 ) {return 1;} //Si el factorial es 0, directamente el resultado es 1
 		else {
 			$resultado = 1;
-			for($i =1; $i <= $num; $i++){
+			for($i =1; $i <= $num; $i++){ //Bucle que va multiplicando
 				$resultado=$resultado*$i;
 			}
 			return $resultado;
